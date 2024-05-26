03b2e2922a1a7dd22424812c189a60ebae450612
parikshith078 <palegar.parikshith@gmail.com>
2024-05-26 13:26:53 +0530

    integrated flag module

--- database/utils.go
+++ database/utils.go
@@ -1,6 +1,12 @@
 package database
 
-import "os/user"
+import (
+	"bufio"
+	"errors"
+	"io"
+	"os/user"
+	"strings"
+)
 
 func GetDotfilePath() string {
 	usr, err := user.Current()
@@ -11,3 +17,24 @@ func GetDotfilePath() string {
 	return path
 }
 
+func getInput(r io.Reader, args ...string) (string, error) {
+
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", errors.New("empty todo is not allowed")
+	}
+
+	return text, nil
+
+}
--- main.go
+++ main.go
@@ -1,28 +1,70 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
 	"pari/passkey-v2/database"
 )
 
 func main() {
+
+	var (
+		set  = flag.Bool("set", false, "Add new key value pair")
+		get  = flag.String("get", "", "Get value using key")
+		del  = flag.String("del", "", "Delete a pair using key")
+		list = flag.Bool("list", false, "List all key value pairs")
+	)
+
+	flag.Parse()
+
 	data := database.DB{}
-	path := database.GetDotfilePath()
-	// err := data.Set("new", "new2")
-	err := data.Load(path)
-	if err != nil {
-		panic(err)
+	filePath := database.GetDotfilePath()
+
+	if err := data.Load(filePath); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
 	}
-	// err = data.Set("tes2", "tes1")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// err, res := data.Get("new")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	data.List()
-	err = data.Store(path)
-	if err != nil {
-		panic(err)
+
+	switch {
+	case *set:
+		args := flag.Args()
+		if len(args) != 2 {
+			fmt.Fprintln(os.Stderr, errors.New("2 arguments only").Error())
+			os.Exit(1)
+		}
+		if err := data.Set(args[0], args[1]); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		if err := data.Store(filePath); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *get != "":
+		err, res := data.Get(*get)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(res.Value)
+
+	case *del != "":
+		if err := data.Delete(*del); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		if err := data.Store(filePath); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *list:
+		data.List()
+
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
 	}
 }
